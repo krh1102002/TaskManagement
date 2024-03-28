4eb60f0f88a377e763197a432c63058c76cba247
Kunal Hulke <hulke1102002@gmail.com>
2024-03-28 16:29:37 +0530

    removing the unwanted command

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,32 @@
-const express = require('express')
-const env = require('dotenv')
-const cors = require('cors')
-const helmet = require('helmet')
-const mongoose = require('mongoose')
-const user = require('./routes/user')
-const task = require('./routes/task')
+const express = require("express");
+const env = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const user = require("./routes/user");
+const task = require("./routes/task");
 
-const app = express()
-env.config()
+const app = express();
+env.config();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(helmet())
-app.use(cors())
-
+app.use(helmet());
+app.use(cors());
 
 // Connection with Mongo Database
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
-    console.log("Database connected")
-}).catch(() =>{
-    console.log("Database connection failed")
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch(() => {
+    console.log("Database connection failed");
+  });
 
-app.use('/user',user)
-app.use('/task',task)
+app.use("/user", user);
+app.use("/task", task);
 
-app.listen(4000,()=>{
-    console.log("Server is Running on PORT 4000")
-})
+app.listen(4000, () => {
+  console.log("Server is Running on PORT 4000");
+});
